4fe75c57d3acd3861400a6934eafbe67f5367e18
Lee <116345037+LeeLawliet@users.noreply.github.com>
2025-04-26 16:39:31 +0200

    FINIUSHEDF WORKING

--- HomeAssignment/Lee_Xerri_PFC_Home/Lee_Xerri_PFC_Home/Program.cs
+++ HomeAssignment/Lee_Xerri_PFC_Home/Lee_Xerri_PFC_Home/Program.cs
@@ -1,18 +1,53 @@
-using Lee_Xerri_PFC_Home.Repositories;
+﻿using Lee_Xerri_PFC_Home.Repositories;
 using Lee_Xerri_PFC_Home.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using StackExchange.Redis;
+using Google.Cloud.SecretManager.V1;
 
 namespace Lee_Xerri_PFC_Home
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Configure authentication
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP(int.Parse(port));
+            });
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            
+            var secretManager = SecretManagerServiceClient.Create();
+            string projectId = "pftchome-457811";
+
+            async Task<string> GetSecret(string secretId)
+            {
+                var name = new SecretVersionName(projectId, secretId, "latest");
+                var result = await secretManager.AccessSecretVersionAsync(name);
+                return result.Payload.Data.ToStringUtf8();
+            }
+
+            var clientId = await GetSecret("Authentication-Google-ClientId");
+            var clientSecret = await GetSecret("Authentication-Google-ClientSecret");
+            var redisPassword = await GetSecret("RedisPassword");
+            var redisUsername = await GetSecret("RedisUsername");
+            var mailGunDomain = await GetSecret("MailGunDomain");
+            var mailGunApiKey = await GetSecret("MailGunApiKey");
+
+            builder.Configuration["Authentication:Google:ClientId"] = clientId;
+            builder.Configuration["Authentication:Google:ClientSecret"] = clientSecret;
+            builder.Configuration["RedisUsername"] = redisUsername;
+            builder.Configuration["RedisPassword"] = redisPassword;
+            builder.Configuration["MailGunDomain"] = mailGunDomain;
+            builder.Configuration["MailGunApiKey"] = mailGunApiKey;
+
+            // Configure Authentication
             builder.Services
                 .AddAuthentication(options =>
                 {
@@ -40,62 +75,34 @@ namespace Lee_Xerri_PFC_Home
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
             });
 
-            if (builder.Environment.IsDevelopment())
-                builder.Configuration.AddUserSecrets<Program>();
-
-            builder.Services.AddSingleton<BucketRepository>();
-            builder.Services.AddSingleton<FirestoreRepository>();
-            builder.Services.AddSingleton<PubSubService>();
-            string connectionRedis = builder.Configuration["Redis"];
-            string usernameRedis = builder.Configuration["RedisUsername"];
-            string passwordRedis = builder.Configuration["RedisPassword"];
-
-            var options = new ConfigurationOptions
+            var optionsRedis = new ConfigurationOptions
             {
                 AbortOnConnectFail = false,
-                User = usernameRedis,
-                Password = passwordRedis,
-                EndPoints = { { "redis-10900.c327.europe-west1-2.gce.redns.redis-cloud.com", 10900 }},
+                User = builder.Configuration["RedisUsername"],
+                Password = builder.Configuration["RedisPassword"],
+                EndPoints = { { "redis-10900.c327.europe-west1-2.gce.redns.redis-cloud.com", 10900 } }
             };
 
             builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
-                    ConnectionMultiplexer.Connect(options));
+                ConnectionMultiplexer.Connect(optionsRedis));
             builder.Services.AddSingleton<RedisRepository>();
+            builder.Services.AddSingleton<BucketRepository>();
+            builder.Services.AddSingleton<FirestoreRepository>();
+            builder.Services.AddSingleton<PubSubService>();
             builder.Services.AddHttpClient<MailGunService>();
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddRazorPages();
-
             var app = builder.Build();
-            
-            // Configure the HTTP request pipeline.
+
+            // Usual Middleware
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.Use(async (context, next) =>
-            {
-                try
-                {
-                    await next();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Global error: " + ex.Message);
-                    Console.WriteLine(ex.StackTrace);
-                    throw;
-                }
-            });
-
             app.UseAuthentication();
             app.UseAuthorization();
 
@@ -103,7 +110,7 @@ namespace Lee_Xerri_PFC_Home
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }
