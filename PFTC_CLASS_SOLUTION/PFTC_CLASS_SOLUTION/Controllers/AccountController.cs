57e20c96f27a2f83a1b4780ba6427b6d09807f0b
Lee <116345037+LeeLawliet@users.noreply.github.com>
2025-04-29 14:05:30 +0200

    Merge branch 'main' of https://github.com/LeeLawliet/IT_SWD_63A_PFC_2025

---
+++ PFTC_CLASS_SOLUTION/PFTC_CLASS_SOLUTION/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PFTC_CLASS_SOLUTION.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            // this is only for logged in users
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        //[HttpGet("login-google")]
+        //public IActionResult LoginWithGoogle()
+        //{
+        //    var redirectUrl = Url.Action("GoogleResponse", "Account");
+        //    var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+        //    return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        //}
+
+        //[HttpGet("signin-google")]
+        //public async Task<IActionResult> GoogleResponse()
+        //{
+        //    var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        //    if (!result.Succeeded)
+        //        return RedirectToAction("Index", "Home");
+
+        //    return RedirectToAction("Index", "Account");
+        //}
+    }
+}
