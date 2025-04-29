33eaf34d55f7d16adcb239c6ebdb0e656b337f7a
Lee <116345037+LeeLawliet@users.noreply.github.com>
2025-04-29 14:05:30 +0200

    Merge branch 'main' of https://github.com/LeeLawliet/IT_SWD_63A_PFC_2025

---
+++ PFTC_CLASS_SOLUTION/PFTC_CLASS_SOLUTION/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PFTC_CLASS_SOLUTION.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.txt
+++
@@ -1,6 +0,0 @@
-GITHUB:
-https://github.com/LeeLawliet/IT_SWD_63A_PFC_2025
-
-HOSTED WEBSITE (Cloud Run):
-https://lee-xerri-pfc-home-1078243225196.europe-west1.run.app/
-
